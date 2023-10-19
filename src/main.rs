500be2b23cb3d1b0a384c07486688bd42d56ab49
galah92 <galaharoni92@gmail.com>
2023-10-19 20:04:04 +0300

    Now can connect to WiFi, except it disconnect due to issues with rx_queue_size config

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #![no_std]
 #![no_main]
 
+use embedded_svc::wifi::{ClientConfiguration, Configuration, Wifi};
 use esp_backtrace as _;
 use esp_println::println;
+use esp_wifi::wifi::WifiMode;
+use esp_wifi::{initialize, EspWifiInitFor};
 use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay, IO};
+use hal::{systimer::SystemTimer, Rng};
 
 #[entry]
 fn main() -> ! {
@@ -21,6 +25,34 @@ fn main() -> ! {
 
     println!("Hello world!");
 
+    let timer = SystemTimer::new(peripherals.SYSTIMER).alarm0;
+    let init = initialize(
+        EspWifiInitFor::Wifi,
+        timer,
+        Rng::new(peripherals.RNG),
+        system.radio_clock_control,
+        &clocks,
+    )
+    .unwrap();
+
+    let (wifi, _) = peripherals.RADIO.split();
+    let (_wifi_interface, mut controller) =
+        esp_wifi::wifi::new_with_mode(&init, wifi, WifiMode::Sta).unwrap();
+    println!("Device capabilities: {:?}", controller.get_capabilities());
+
+    const SSID: &str = env!("SSID");
+    const PASSWORD: &str = env!("PASSWORD");
+    let client_config = Configuration::Client(ClientConfiguration {
+        ssid: SSID.into(),
+        password: PASSWORD.into(),
+        ..Default::default()
+    });
+    println!("Setting configuration: {:?}", &client_config);
+    controller.set_configuration(&client_config).unwrap();
+    controller.start().unwrap();
+    println!("Wifi started: {}", controller.is_started().unwrap());
+    controller.connect().unwrap();
+
     // Set GPIO7 as an output, and set its state high initially.
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let button = io.pins.gpio9.into_pull_up_input();
@@ -30,7 +62,7 @@ fn main() -> ! {
     let mut i = 0;
     loop {
         if button.is_high().unwrap() {
-            println!("Loop... {}", i);
+            println!("Loop... {} {:?}", i, controller.is_connected());
             led.toggle().unwrap();
         }
         i += 1;
